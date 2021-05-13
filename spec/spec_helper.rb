15ca2f7c77f6c50df682078f486d44d66946f213
Mandeep Khinda <mkhinda29@users.noreply.github.com>
2021-05-13 17:31:11 -0400

    tweaks 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,16 @@
 require 'simplecov'
-# require 'simplecov-lcov'
-# SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-# SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+require 'simplecov-lcov'
 
-# SimpleCov::Formatter::LcovFormatter.config do |c|
-#   c.single_report_path = 'coverage/lcov.info'
-# end
-
-# SimpleCov.start 'rails'
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
+end
 
 require 'coveralls'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
                                                                  SimpleCov::Formatter::HTMLFormatter,
-                                                                 Coveralls::SimpleCov::Formatter
+                                                                 Coveralls::SimpleCov::Formatter,
+                                                                 SimpleCov::Formatter::LcovFormatter
                                                                ])
 
 SimpleCov.start 'rails'
