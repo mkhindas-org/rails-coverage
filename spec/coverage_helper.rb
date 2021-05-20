e830ba478bf8a01a8a7ea8b99e4bc0b8f2c3ff4c
Mandeep Khinda <mkhinda29@users.noreply.github.com>
2021-05-20 15:27:02 -0400

    ugh

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-# require 'simplecov-lcov'
-require 'simplecov-cobertura'
+require 'simplecov-lcov'
+# require 'simplecov-cobertura'
 
 # SimpleCov.formatter =
 
-# SimpleCov::Formatter::LcovFormatter.config do |c|
-#   c.report_with_single_file = true
-#   c.single_report_path = 'coverage/lcov.info'
-# end
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
+end
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
                                                                  SimpleCov::Formatter::HTMLFormatter,
-                                                                 #  SimpleCov::Formatter::LcovFormatter
-                                                                 SimpleCov::Formatter::CoberturaFormatter
+                                                                 SimpleCov::Formatter::LcovFormatter
+                                                                 #  SimpleCov::Formatter::CoberturaFormatter
                                                                ])
 
 # SimpleCov.minimum_coverage 80
 # SimpleCov.maximum_coverage_drop 25
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  track_files '{app,lib}/**/*.rb'
+  coverage_dir 'coverage'
+  enable_coverage :branch
+end
